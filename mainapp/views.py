b6e16aa35b0e039dba17b39e04dd31c2ccdd49a1
mhd <m.almouhtaseb@gmail.com>
2024-05-04 15:24:30 +0300

    music-

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
-!python3 -m pip install -U git+https://github.com/facebookresearch/audiocraft#egg=audiocraft
-# !python3 -m pip install -U audiocraft
+# # Create your views here.
+# !python3 -m pip install -U git+https://github.com/facebookresearch/audiocraft#egg=audiocraft
+# # !python3 -m pip install -U audiocraft
 
-from audiocraft.models import musicgen
-from audiocraft.utils.notebook import display_audio
-import torch
+# from audiocraft.models import musicgen
+# from audiocraft.utils.notebook import display_audio
+# import torch
 
 
-model = musicgen.MusicGen.get_pretrained('medium', device='cuda')
-model.set_generation_params(duration=20)
+# model = musicgen.MusicGen.get_pretrained('medium', device='cuda')
+# model.set_generation_params(duration=20)
 
-res = model.generate([
-    'syrian traditional music'
-], 
-    progress=True)
+# res = model.generate([
+#     'syrian traditional music'
+# ], 
+#     progress=True)
