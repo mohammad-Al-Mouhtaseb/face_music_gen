969433f75d1f73562941d9ffafc36f800b6ebc36
mhd <m.almouhtaseb@gmail.com>
2024-05-06 11:36:49 +0300

    bnm

--- mainapp/views.py
+++ mainapp/views.py
@@ -5,9 +5,9 @@ import requests
 from transformers import AutoProcessor, MusicgenForConditionalGeneration, pipeline
 # import scipy
 
-synthesiser=pipeline("text-to-audio","facebook/musicgen-small")
-# model = MusicgenForConditionalGeneration.from_pretrained("facebook/musicgen-small")
-# processor = AutoProcessor.from_pretrained("facebook/musicgen-small")
+# synthesiser=pipeline("text-to-audio","facebook/musicgen-small")
+model = MusicgenForConditionalGeneration.from_pretrained("facebook/musicgen-small")
+processor = AutoProcessor.from_pretrained("facebook/musicgen-small")
 # import torch
 # device = "cuda:0" if torch.cuda.is_available() else "cpu"
 # device="cpu"
