91206725ccbcaed3a34308810fb1664c3a66341b
mhd <m.almouhtaseb@gmail.com>
2024-05-09 20:20:51 +0300

    asd

--- mainapp/views.py
+++ mainapp/views.py
@@ -6,7 +6,7 @@ import requests
 # warnings.simplefilter(action='ignore', category=FutureWarning)
 
 # from transformers import AutoProcessor, MusicgenForConditionalGeneration
-import scipy
+# import scipy
 
 # model = MusicgenForConditionalGeneration.from_pretrained("facebook/musicgen-small", force_download=True)
 # processor = AutoProcessor.from_pretrained("facebook/musicgen-small", force_download=True)
@@ -15,14 +15,12 @@ import scipy
 # device = "cuda:0" if torch.cuda.is_available() else "cpu"
 # model.to(device)
 # sampling_rate = model.config.audio_encoder.sampling_rate
+import scipy
+from audiocraft.models import MusicGen
+from audiocraft.data.audio import audio_write
+model = MusicGen.get_pretrained("small")
+model.set_generation_params(duration=8)  # generate 8 seconds.
 
-
-from audiocraft.models import musicgen
-from audiocraft.utils.notebook import display_audio
-import torch
-device = "cuda" if torch.cuda.is_available() else "cpu"
-model = musicgen.MusicGen.get_pretrained('medium', device=device)
-model.set_generation_params(duration=8)
 
 def gen(request,text):
     # inputs = processor(
@@ -30,18 +28,21 @@ def gen(request,text):
     #     padding=True,
     #     return_tensors="pt",
     # )
-    res = model.generate([
-        'crazy EDM, heavy bang'
-    ], 
-        progress=True)
-    display_audio(res, 32000)
     # audio_values = model.generate(**inputs.to(device), do_sample=True, guidance_scale=3, max_new_tokens=256)
-    scipy.io.wavfile.write("mainapp/music/m1.wav", data=res)
+    # scipy.io.wavfile.write("mainapp/music/m1.wav", rate=sampling_rate, data=audio_values[0, 0].cpu().numpy())
+    descriptions = ["happy rock", "energetic EDM"]
+
+    wav = model.generate(descriptions)  # generates 2 samples.
+
+    for idx, one_wav in enumerate(wav):
+        # Will save under {idx}.wav, with loudness normalization at -14 db LUFS.
+        audio_write(f'{idx}', one_wav.cpu(), model.sample_rate, strategy="loudness")
+    scipy.io.wavfile.write("11.wav", rate=model.sample_rate, data=wav[0, 0].cpu().numpy())
     return JsonResponse({"res":"sucsess"})
 
 def get(request,text):
     try:
-        m = open("mainapp/music/m1.wav", 'rb')
+        m = open("11.wav", 'rb')
         response = FileResponse(m)
         return response
     except:
